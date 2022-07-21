370938b2e0d2764447e439387421fff3374ef9af
Jiwon8037 <lego2090@naver.com>
2022-07-21 23:34:00 +0900

    미들웨어 수정

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -8,8 +8,15 @@ const posts=new Router();
 posts.get('/',list);
 posts.post('/',checkLoggedIn,write);
 
-posts.get('/:id',checkOwnPost,getPostById,read);
-posts.delete('/:id',checkLoggedIn,checkOwnPost,getPostById,remove);
-posts.patch('/:id',checkLoggedIn,checkOwnPost,getPostById,update);
+posts.get('/:id',getPostById,read);
+posts.delete('/:id',getPostById,checkLoggedIn,checkOwnPost,remove);
+posts.patch('/:id',getPostById,checkLoggedIn,checkOwnPost,update);
+
+// const post=new Router();
+// post.get('/',read);
+// post.delete('/',checkLoggedIn,checkOwnPost,remove);
+// post.patch('/',checkLoggedIn,checkOwnPost,update);
+
+// posts.use('/:id',getPostById,post.routes());
 
 module.exports = posts;
